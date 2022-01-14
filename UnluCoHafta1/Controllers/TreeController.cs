b5f58af514af34384d58cc9189f4cc1c0105b07a
kodluyoruz <arslanon@outlook.com>
2022-01-15 00:21:00 +0300

    refactor and add edit  and sort function

--- UnluCoHafta1/Controllers/TreeController.cs
+++ UnluCoHafta1/Controllers/TreeController.cs
@@ -16,13 +16,13 @@ namespace UnluCoHafta1.Controllers
         {
             new Tree{ID=1,Age=2,Name="Kızılçam",Note="Akdeniz ikliminin müşir türlerinden olup tipik bir ışık ağacıdır"},
             new Tree{ID=2,Age=5,Name="Karaçam",Note="Çamgiller familyasından bir çam türü."},
-            new Tree{ID=3,Age=3,Name="Titrek kavak",Note=" söğütgiller familyasından 25 m'ye kadar boylanabilen, silindirik gövde, sık dallı, geniş konik tepeye sahip bir kavak türü."},
-            new Tree{ID=4,Age=8,Name="İspi meşesi",Note="Kayıngiller familyasından Türkiye'ye özgü endemik meşe alt türü."},
-            new Tree{ID=5,Age=2,Name="Macar meşesi",Note="40 m'ye kadar boy yapabilen kalın dallı geniş tepeli büyükçe bir ağaçtır. Gövde kabukları yukarıdan aşağıya doğru çatlaklı haldedir."}
+            new Tree{ID=5,Age=3,Name="Titrek kavak",Note=" söğütgiller familyasından 25 m'ye kadar boylanabilen, silindirik gövde, sık dallı, geniş konik tepeye sahip bir kavak türü."},
+            new Tree{ID=3,Age=8,Name="İspi meşesi",Note="Kayıngiller familyasından Türkiye'ye özgü endemik meşe alt türü."},
+            new Tree{ID=4,Age=2,Name="Macar meşesi",Note="40 m'ye kadar boy yapabilen kalın dallı geniş tepeli büyükçe bir ağaçtır. Gövde kabukları yukarıdan aşağıya doğru çatlaklı haldedir."}
 
         };
 
-        [HttpGet("{id}")]
+        [HttpGet("get/{id}")]
         public IActionResult GetById(int id) // Route 
         {
             var tree = trees.SingleOrDefault(item => item.ID == id);
@@ -32,7 +32,7 @@ namespace UnluCoHafta1.Controllers
             return Ok(tree);
         }
 
-        [HttpGet("GetByIdWithQuery")]
+        [HttpGet("getByIdWithQuery")]
         public IActionResult GetByIdWithQuery([FromQuery] int id) // Query
         {
             var tree = trees.SingleOrDefault(item => item.ID == id);
@@ -42,7 +42,7 @@ namespace UnluCoHafta1.Controllers
             return Ok(tree);
         }
 
-        [HttpGet("GetAll")]
+        [HttpGet("getAll")] // Oluşturma sırasına göre sıralamakta
         public IActionResult GetAll()
         {
             if (trees.Count == 0)
@@ -51,7 +51,7 @@ namespace UnluCoHafta1.Controllers
             return Ok(trees);
         }
 
-        [HttpPost]
+        [HttpPost("create")]
         public IActionResult Create([FromBody] Tree newTree)
         {
             var tree = trees.SingleOrDefault(item => item.ID == newTree.ID);
@@ -62,8 +62,8 @@ namespace UnluCoHafta1.Controllers
             return Created("Ağaç eklendi.",newTree);
         }
 
-        [HttpPatch("/editNote/{id}")]
-        public IActionResult EditNote(int id, string note)
+        [HttpPatch("editNote/{id}")]
+        public IActionResult EditNote(int id, [FromBody]string note)
         {
             var editTree = trees.Find(item => item.ID == id);
             if (editTree is null)
@@ -75,7 +75,7 @@ namespace UnluCoHafta1.Controllers
             return Ok();
         }
 
-        [HttpDelete("/delete/{id}")]
+        [HttpDelete("delete/{id}")]
         public IActionResult Delete(int id)
         {
             var deleteTree = trees.Find(item => item.ID == id);
@@ -85,5 +85,49 @@ namespace UnluCoHafta1.Controllers
             return Ok();
         }
         
+        [HttpPut("edit/{id}")]
+        public IActionResult Edit(int id,[FromBody] Tree tree)
+        {
+            var editTree = trees.Find(item => item.ID == id);
+            if (editTree is null)
+                return BadRequest("Ağaç mevcut değil");
+            editTree.Name = tree.Name;
+            editTree.Age = tree.Age;
+            editTree.Note = tree.Note;
+
+            return Ok($"{id}'ye sahip film düzenlendi.");
+        }
+
+        [HttpGet("sortAscByName")] // isme göre artan sıralama
+        public IActionResult SortAscByName()
+        {
+            if (trees.Count <= 0)
+                return NotFound("Ağaçlar bulunamadı.");
+            return Ok(trees.OrderBy(tree => tree.Name));
+        }
+
+        [HttpGet("sortDescByName")] // isme göre azalan sıralama
+        public IActionResult SortDescByName()
+        {
+            if (trees.Count <= 0)
+                return NotFound("Ağaçlar bulunamadı.");
+            return Ok(trees.OrderByDescending(tree => tree.Name));
+        }
+
+        [HttpGet("sortAscByID")] // ID göre artan sıralama
+        public IActionResult SortAscByID()
+        {
+            if (trees.Count <= 0)
+                return NotFound("Ağaçlar bulunamadı.");
+            return Ok(trees.OrderBy(tree => tree.ID));
+        }
+
+        [HttpGet("sortDescByID")] // ID göre azalan sıralama
+        public IActionResult SortDescByID()
+        {
+            if (trees.Count <= 0)
+                return NotFound("Ağaçlar bulunamadı.");
+            return Ok(trees.OrderByDescending(tree => tree.ID));
+        }
     }
 }
